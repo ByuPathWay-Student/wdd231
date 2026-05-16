43064d9f6132d5299edd1d2f5e7e843f75404931
ByuPathWay-Student <ascott@byupathway.edu>
2026-05-15 20:04:26 -0600

    fixed a logo not working

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,7 +1,39 @@
+const url = 'https://byupathway-student.github.io/wdd231/chamber/data/members.json';
 const cards = document.querySelector('#cards');
-// const datafile = 
+
+const displayCompanies = (companies) => {
+    companies.forEach((company) => {
+        let card = document.createElement('section');
+        let name = document.createElement('h2');
+        let logo = document.createElement('img');
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+        let website = document.createElement('p');
+
+        name.textContent = `${company.companyName}`;
+
+        logo.setAttribute('src', company.companyImage);
+        logo.setAttribute('alt', `Logo for ${company.companyName}`);
+        logo.setAttribute('loading', 'lazy');
+
+        address.textContent = `${company.companyAddress}`;
+        phone.textContent = `${company.companyPhone}`;
+        website.textContent = `${company.companyUrl}`;
+
+        card.appendChild(logo);
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+        cards.appendChild(card);
+    });
+}
 
 
-// async function getCompanyData() {
-//     const response = await fetch()
-// }
+async function getCompanyData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayCompanies(data.companies);
+}
+
+getCompanyData();
