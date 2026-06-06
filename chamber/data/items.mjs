55323abba49a1d5b61354adecebd7d433bb28aa1
ByuPathWay-Student <ascott@byupathway.edu>
2026-06-05 18:46:53 -0600

    started the discover page

---
+++ chamber/data/items.mjs
@@ -0,0 +1,52 @@
+export const itemsOfInterest = [
+    {
+        "photo": "",
+        "name": "Great Salt Lake",
+        "address": "13312 W 1075 S Magna UT",
+        "description": "A large salt water lake in Utah, it's home to many creatures despite how much salt there is."
+    },
+    {
+        "photo": "",
+        "name": "Canyonlands National Park",
+        "address": "Grand View Point Rd Moab UT",
+        "description": "A large desert area with many intricate rock formations. Home to the popular mesa arch."
+    },
+    {
+        "photo": "",
+        "name": "Utah's Hogle Zoo",
+        "address": "2600 Sunnyside Ave S Salt Lake City UT",
+        "description": "A large zoo in Utah. They have one of the highest animal care standards in the world, and help with animal conservation."
+    },
+    {
+        "photo": "",
+        "name": "Natural History Museum of Utah",
+        "address": "301 Wakara Way Salt Lake City UT",
+        "description": "This museum exhibits a lot about natural history in Utah, including lots of dinosaur fossils."
+    },
+    {
+        "photo": "",
+        "name": "Snowbird",
+        "address": "9385 Snowbird Ctr Dr Snowbird UT",
+        "description": "A popular ski and snowboard resort that also has summer activities."
+    },
+    {
+        "photo": "",
+        "name": "Goblin Valley State Park",
+        "address": "18630 Goblin Valley Rd Green River UT",
+        "description": "An area with a bunch of odd goblin shaped rocks."
+    },
+    {
+        "photo": "",
+        "name": "Arches National Park",
+        "address": "Grand County UT",
+        "description": "A national park home to many sandstone arches, the most well known being delicate arch."
+    },
+    {
+        "photo": "",
+        "name": "Bonneville Salt Flats",
+        "address": "Tooele County UT",
+        "description": "The Bonneville Salt Flats are a large salt crust that are the remains of prehistoric Lake Bonneville that dried up."
+    },
+    
+]
+        
