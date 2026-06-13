13249091258db01ad5723a01ec3f06820a232db4
ByuPathWay-Student <ascott@byupathway.edu>
2026-06-13 11:10:22 -0600

    added the video link

--- final/scripts/thanks.js
+++ final/scripts/thanks.js
@@ -10,4 +10,4 @@ aPrompt.textContent = randomPrompt;
 
 document.querySelector('#recipt').innerHTML = `<p><strong>Welcome to the club, ${memberInfo.get('first')} ${memberInfo.get('last')}!</strong></p>
 <p><strong>Your email:</strong> ${memberInfo.get('emailAdd')}</p>
-<p><strong>Subscribed to newsletter:</strong> ${memberInfo.get('newsl')}</p>`
+<p><strong>Subscribed to newsletter:</strong> ${memberInfo.get('newsl')}</p>`   
